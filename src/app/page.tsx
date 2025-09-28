1b0c6f0b38b6f0e69d40ae0fc1946a0388e6c434
市川凌大 <ichikawaryohta@MacBook-Air.local>
2025-09-28 10:23:51 +0900

    create Display component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Displey from '@/components/Display/Displey';
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <>
       <h1 className={styles.title}>Welcom to my Next.js Project Page!</h1>
+      <Displey />
     </>
   );
 }
---
+++ src/components/Display/Displey.tsx
@@ -0,0 +1,7 @@
+import styles from './Display.module.css';
+
+export default function Displey() {
+  return (
+    <div className={styles.display}>Displey component</div>
+  )
+}
