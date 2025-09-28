3dfe339b7d87580a2b2b28944960ab746ea0b565
市川凌大 <ichikawaryohta@MacBook-Air.local>
2025-09-28 18:09:38 +0900

    add page

--- src/components/Display/Displey.tsx
+++ src/components/Display/Displey.tsx
@@ -2,6 +2,10 @@ import styles from './Display.module.css';
 
 export default function Displey() {
   return (
-    <div className={styles.display}>Displey component</div>
+      <div className={styles.display}>
+        <p>
+          Displey component
+        </p>
+      </div>
   )
 }
