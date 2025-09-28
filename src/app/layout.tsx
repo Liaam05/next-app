99b9dbb637a4fc40d8aea3eff3dbcd70ab9bd18a
市川凌大 <ichikawaryohta@MacBook-Air.local>
2025-09-28 09:41:58 +0900

    add header footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>Next.jsプロジェクト</header>
         {children}
+        <footer>fin.</footer>
       </body>
     </html>
   );
